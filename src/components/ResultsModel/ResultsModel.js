09dd9bcd8899200bf4a8c333d6e2bc3fb1e43eae
Irene R <irene@smallwondermedia.com>
2019-01-31 14:44:28 -0500

    added date to ResultsModel

--- src/components/ResultsModel/ResultsModel.js
+++ src/components/ResultsModel/ResultsModel.js
@@ -4,10 +4,11 @@ const ResultsModel = (props) => (
 
     <div className="individualResult">
         <div className="headline">{props.title}</div>
+        <div className="date">{props.date}</div>
         <a href={props.url} target="_blank" rel="noopener noreferrer">
         Read story</a>
     </div>
-    
+
 )
 
 export default ResultsModel;
