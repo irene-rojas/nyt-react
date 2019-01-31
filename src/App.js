f25292397971eda717cecd9481ba8fd707fa812f
Irene R <irene@smallwondermedia.com>
2019-01-30 23:11:27 -0500

    added header

--- src/App.js
+++ src/App.js
@@ -1,14 +1,59 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
 class App extends Component {
+
+    state = {
+        query: "",
+        startYear: "",
+        endYear: "",
+        articles: []
+        // empty array waiting for results
+    };
+
+    // text fields
+    onChange = (event) => {
+        this.setState({
+            query: event.target.value
+        });
+    }
+
   render() {
     return (
-      <div className="App">
+    
+        <div className="parallax">
+            
+        <div className="App">
+
+            <div className="header">
+                <h1>'The New York Times' Article Search</h1>
+                <p>Search for articles from "The New York Times"</p>
+            </div>
+
+            {/* <div className="searchDiv">
+                <h2 id="searchWord">Search</h2>
+                <Search 
+                query={this.state.query} 
+                handleInputChange={this.handleInputChange} 
+                handleFormSubmit={this.handleFormSubmit} 
+                startYear={this.state.startYear} 
+                endYear={this.state.endYear}/>
+            </div> */}
+
+            {/* {this.state.articles.length > 0 && 
+                <div className="resultsDiv">
+                <h2 
+                    id="resultWord">Results</h2>
+                <Results 
+                    articles={this.state.articles} 
+                    handleArticleSave={this.handleArticleSave}/>
+            </div>} */}
 
 
+        </div>
 
-      </div>
+        </div>
     );
   }
 }
