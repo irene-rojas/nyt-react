67820cd76d8a5ac4a26a7d4da41d074a0a103f05
Irene Rojas <irene@smallwondermedia.com>
2019-09-11 20:45:11 -0400

    Updates

---
+++ precache-manifest.d1fdb0a1d7b8ece84f266b8df08e291a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9faec1824e47ef0cb05f",
+    "url": "/nyt-react/static/css/main.f00d5d51.chunk.css"
+  },
+  {
+    "revision": "9faec1824e47ef0cb05f",
+    "url": "/nyt-react/static/js/main.9faec182.chunk.js"
+  },
+  {
+    "revision": "ced3fa14406e74805fe3",
+    "url": "/nyt-react/static/js/1.ced3fa14.chunk.js"
+  },
+  {
+    "revision": "dd758aa3fb4cf82333cb",
+    "url": "/nyt-react/static/js/runtime~main.dd758aa3.js"
+  },
+  {
+    "revision": "b283ac5b6c27e7dd7ca6e19306120681",
+    "url": "/nyt-react/index.html"
+  }
+];
